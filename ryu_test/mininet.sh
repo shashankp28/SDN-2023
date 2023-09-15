c88c1139fec26b002622f315881086f93d478bbd
Shashank P <shashankp2832@gmail.com>
2023-09-15 10:39:38 +0530

    Added rulebook

---
+++ ryu_test/mininet.sh
@@ -0,0 +1,1 @@
+sudo mn --controller remote,ip=127.0.0.1:6640 --switch ovsk,protocols=OpenFlow13 --mac --ipbase=10.1.1.0/24 --topo single,4
---
+++ ryu_test/ryu.sh
@@ -0,0 +1,2 @@
+wget https://github.com/iwaseyusuke/docker-ryu-mininet/raw/master/docker-compose.yml
+docker-compose run --rm ryu-mininet
